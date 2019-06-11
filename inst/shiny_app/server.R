ab7bdb521a42b5854104e1d101559b90384c48e5
Jaime Thompson <jathompson@WEST-INC.LOCAL>
2019-06-10 21:24:03 -0600

    modification to neural net output

--- inst/shiny_app/server.R
+++ inst/shiny_app/server.R
@@ -13,8 +13,8 @@ appScripts <- system.file("shiny_app", package="finFindR")
 sapply(list.files(path=appScripts,pattern="*_serverside.R",full.names = T),source,.GlobalEnv)
 
 networks <- system.file("extdata", package="finFindR")
-pathNet <- mxnet::mx.model.load(file.path(networks,'tracePath128'), 20)
-cropNet <- mxnet::mx.model.load(file.path(networks,'cropperInit'), 940)
+pathNet <- mxnet::mx.model.load(file.path(networks,'tracePath128'), 21)
+cropNet <- mxnet::mx.model.load(file.path(networks,'cropperInit'), 941)
 mxnetModel <- mxnet::mx.model.load(file.path(networks,'fin_triplet32_4096_final'), 5600)
 
 
